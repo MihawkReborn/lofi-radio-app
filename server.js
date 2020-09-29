0357aa831f542362228e61ca8b102d1e1dadce4a
MihawkReborn <63207003+MihawkReborn@users.noreply.github.com>
2020-09-29 16:19:11 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const https = require("https");
+const jsonServer = require('json-server')
+const data = require('./db.json')
+
+const port = process.env.PORT || 4000;
+const router = jsonServer.router(data)
+const server = jsonServer.create()
+
+server.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+server.use(jsonServer.defaults(['./public']))
+server.use(router)
+server.listen(port)
+
+console.log('Server is running on:', 'http://localhost:' + port);
